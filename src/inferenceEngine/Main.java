8fcaf76509aa1f18dc24f4396d850ce3709b64a1
Emperor-Justinian <emperorjustinian_i-github@yahoo.com.au>
2017-05-07 22:13:04 +1000

    Replace main class in default package with main class in application-specific package

---
+++ src/inferenceEngine/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package inferenceEngine;
+
+/**
+ * @author andrew
+ *
+ */
+public class Main {
+
+  /**
+   * 
+   */
+  public Main() {
+    // TODO Auto-generated constructor stub
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
